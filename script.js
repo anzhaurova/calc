0f61230bac20cbb0a8b414ef10f26db11432cc72
Vika Anzhaurova <anzhavi@gmail.com>
2019-05-17 18:08:08 +0300

    added calculation logic.need to check the purity of the code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-<script> 
-"use strict"; //использовать строгий ES5
+
 function calculate() {
 	//найти элементы ввода и вывода в документе
 	var amount = document.getElementById("amount");
 	var apr = document.getElementById("apr");
-	var years = document.getElementById("months");
+	var years = document.getElementById("years");
 	var payment = document.getElementById("payment");
+	var total = document.getElementById("total");
 	var totalinterest = document.getElementById("totalinterest");
 	/*получить введенные пользов-лем данные
 	преобразовать процентную ставку из процентов в десятичное число и преобр
@@ -43,4 +43,28 @@ function calculate() {
 		totalinterest.innerHTML = "";
 	}
 }
-</script>
+//сохранить ввод поль-ля в св-вах объекта localStorage
+
+function save(amount,apr, years) {
+	if (window.localStorage) { //выполнить сохранение если поддерж-ся
+		localStorage.loan_amount = amount;
+		localStorage.loan_apr = apr;
+		localStorage.loan_years = years;
+	}
+}
+//автоматически восстановить поля ввода при загрузке документа
+window.onload = function() {
+	//если браузер поддерживает localStorage и имеются сохраненные данные
+	if (window.localStorage && localStorage.loan_amount) {
+		document.getElementById("amount").value = localStorage.loan_amount;
+		document.getElementById("apr").value = localStorage.loan_apr;
+		document.getElementById("years").value = loan_years;
+
+	}
+};
+//передать ввод пользователя серверному сцен кот может возвращать
+//список ссылок
+
+
+
+
